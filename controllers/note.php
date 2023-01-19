e287bfa71575e2e0c0ad45c829bed5975ed7bdf9
luqman73 <luqmanhazli@gmail.com>
2023-01-19 08:48:52 +0800

    EP23 - Added simple Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,8 +4,16 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
 
+if (! $note){
+    abort();
+}
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
