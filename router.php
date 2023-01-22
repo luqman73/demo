8a4bde419b36f89133dc0a0b9811e1865380fa43
luqman73 <luqmanhazli@gmail.com>
2023-01-23 04:01:16 +0800

    EP25 - Added form with tailwind style CSS

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$routes = require('routes.php');
 
 function routeToController($uri, $routes){
 
@@ -28,5 +20,7 @@ function abort($code = 404){
 
     die();
 }
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 routeToController($uri, $routes);
--- views/notes.view.php
+++ views/notes.view.php
@@ -5,16 +5,19 @@
     
     <main>
         <div class="mx-auto max-w-7xl py-6 ssm:px-6 lg:px-8">
+            <ul>
+                <?php foreach ($notes as $note) : ?>
+                    <li>
+                        <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                            <?= $note['body'] ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
 
-            <p>Hello. Welcome to the notes page.</p>
-            <?php foreach ($notes as $note) : ?>
-                <li>
-                    <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
-                        <?= $note['body'] ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-
+            <p class="mt-6">
+                <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
+            </p>
         </div>
     </main>
 
