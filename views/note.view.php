f6a652f572af96cf873e20999cdc27a32ab4fb29
luqman73 <luqmanhazli@gmail.com>
2023-01-24 10:37:05 +0800

    EP26 - Sanitize user input

--- views/note.view.php
+++ views/note.view.php
@@ -8,7 +8,7 @@
             <a href="/notes" class="text-blue-500 underline">Back</a>
         </p>     
                 
-        <p><?= $note['body'] ?></p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 
